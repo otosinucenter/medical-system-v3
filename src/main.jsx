29940902383d5e9016e311f3756f42e003756655
walter florez <wflorez16@gmail.com>
2025-11-30 23:25:54 -0500

    Chore: Use env var for Sentry DSN

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://879893dc65c36bc7c08a99f600e3d668@o4510457470779392.ingest.us.sentry.io/4510457476349952",
+  dsn: import.meta.env.VITE_SENTRY_DSN,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
