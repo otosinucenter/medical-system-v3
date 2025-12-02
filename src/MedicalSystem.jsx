c4ca297fdd5b09392a582d3b8afaf470b9e19a30
walter florez <wflorez16@gmail.com>
2025-12-02 00:24:12 -0500

    fix: missing CheckCircle2 import

--- src/MedicalSystem.jsx
+++ src/MedicalSystem.jsx
@@ -6,7 +6,7 @@ import {
   Trash2, ListPlus, Pill, Ear, Smile, Mic2, Printer, Baby, Info,
   HelpCircle, Download, AlertCircle, History, Clock, Eye, MapPin,
   Briefcase, Mail, AlertTriangle, CalendarDays, FileSignature, Edit3, X, LogOut, MessageCircle, Link,
-  RefreshCw, ChevronUp, ChevronDown, ArrowRight
+  RefreshCw, ChevronUp, ChevronDown, ArrowRight, CheckCircle2
 } from 'lucide-react';
 import { supabase } from './supabaseClient';
 import { createClient } from '@supabase/supabase-js';
